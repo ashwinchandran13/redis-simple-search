7aefae63e3ad2bf4f0fb7122a1ce22453a312884
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-02-20 20:18:35 +0530

    added styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import CarForm from '../lib/CarForm'
+import SearchForm from '../lib/SearchForm'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Create a Car</h1>
+    <div className='overflow-hidden grid place-items-center mt-8'>
+      <h1 className='text-5xl mb-10'>Simple Search Using Redis OM</h1>
+      <div className='grid grid-cols-2 gap-14'>
       <CarForm />
+      <SearchForm />
+      </div>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./lib/**/*.tsx", "./pages/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
